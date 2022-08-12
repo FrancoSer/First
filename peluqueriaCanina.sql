168e718fca94b94ad0640af6f9de9f5a4677bcac
Franco Serafini <francoserafini70@gmail.com>
2022-08-12 19:47:59 -0300

    Entrega final: Integrador final 2022

--- peluqueriaCanina.sql
+++ peluqueriaCanina.sql
@@ -1,4 +1,5 @@
-use peluqueria_canina;
+create database if not exists peluqueriaCanina;
+use peluqueriaCanina;
 create table dueno(
 dni int not null unique,
 nombre varchar (30) not null,
@@ -10,7 +11,7 @@ constraint pk_dni primary key (dni)
 create table perro(
 id_perro int auto_increment not null,
 nombre_p varchar (30) not null,
-fecha_nac varchar (10),
+fecha_nac date,
 sexo varchar (10) not null,
 dni_dueno int,
 constraint pk_id_p primary key (id_perro),
@@ -18,16 +19,15 @@ constraint fk_dni_d foreign key (dni_dueno) references dueno (dni)
 );
 create table historial(
 id_historial int auto_increment not null,
-fecha_h varchar (10) not null,
+fecha_h date,
 id_perro_h int,
 descripcion varchar (300),
 monto int not null,
 constraint pk_id_h primary key (id_historial),
 constraint fk_id_p foreign key (id_perro_h) references perro (id_perro)
 );
-
 insert into dueno (dni, nombre, apellido, telefono, direccion) values ('35648345', 'Franco', 'Serafini', '3415555555', 'Paraguay 2473');
-insert into perro (nombre_p, fecha_nac, sexo, dni_dueno) values ('Almendra', '11/04/2009', 'fenenino', '35648345');
+insert into perro (nombre_p, fecha_nac, sexo, dni_dueno) values ('Almendra', 11/04/2009, 'fenenino', '35648345');
 
 select nombre from dueno where nombre="Pedro";
 -- (para verificar)select * from dueno where nombre="Franco";
