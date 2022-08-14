459a81b87998e82bf651d46ed6a71e7df0ad8216
janu-ss <j.stranieri.s@gmail.com>
2022-08-14 19:29:54 -0300

    Se sube ejercicio de Python a mi Branch

---
+++ puppy.py
@@ -0,0 +1,12 @@
+Historial = (2350,5960,23000,1000,8900)
+resultado = sum(Historial)
+print("El total de los gastos de Puppy es: $", resultado)
+if resultado < 30000:
+    print(resultado)
+else:
+    print("los gastos estan por encima de lo presupuestado")
+
+
+
+
+
